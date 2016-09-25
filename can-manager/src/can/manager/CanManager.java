048470bd74d8c44358026b93d68a09258ff95e1d
Adrien <adrien@sion>
2016-09-25 19:22:51 +0200

    Add querySQL

--- can-manager/src/can/manager/CanManager.java
+++ can-manager/src/can/manager/CanManager.java
@@ -1,13 +1,18 @@
 package can.manager;
 
+import can.manager.data.QuerySQL;
 import java.io.IOException;
 import java.sql.SQLException;
 
 public class CanManager {
 
     public static void main(String[] args) throws SQLException, IOException, InterruptedException{
-
+        
         String fileName;
+        
+        /*
+        --- USE exportDBFtoSQLite ---
+
         
         if(args.length > 0)
             fileName = args[0];
@@ -17,7 +22,22 @@ public class CanManager {
         ExportDBFtoSQLite run = new ExportDBFtoSQLite(fileName);
         
         System.out.println("\nPress ENTER to close de console");
-        //int read = System.in.read();
+        int read = System.in.read();
+        */
 
+        //--- USE querySQL ---
+        
+        if(args.length > 0)
+            fileName = args[0];
+        else
+            fileName = System.getProperty("user.home") + System.getProperty("file.separator") + "Desktop" + System.getProperty("file.separator") + "can.db";
+        
+        QuerySQL data = new QuerySQL(fileName);
+        
+
+        
+        //http://code.makery.ch/library/javafx-8-tutorial/fr/part1/
+        
+        
    }
 }
